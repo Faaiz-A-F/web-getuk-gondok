2b8f8fb854a2a604b21449c621d7c24e4a28c287
Stazer <162255924+ALFANHANIF23115804@users.noreply.github.com>
2026-07-14 20:38:33 +0800

    update opacity bg account page

--- src/components/pages/AccountPage.tsx
+++ src/components/pages/AccountPage.tsx
@@ -569,7 +569,7 @@ export function AccountPage() {
           alt="Magelang"
           fill
           priority
-          className="object-cover object-center"
+          className="object-cover object-center opacity-50"
         />
       </div>
 
